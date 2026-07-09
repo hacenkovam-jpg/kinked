47888c39b6505455b4adf817856abe1a341217fd
hacenkovam-jpg <hacenkovam@gmail.com>
2026-07-09 21:48:50 +0300

    update main

--- probbot/keyboards.py
+++ probbot/keyboards.py
@@ -3,7 +3,8 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMar
 btnProfile = KeyboardButton(text='🔎ИСКАТЬ ФИЛЬМ ПО КОДУ')
 profileKeyboard = ReplyKeyboardMarkup(keyboard=[[btnProfile]], resize_keyboard=True)
 
-btnUrlChannel = InlineKeyboardButton(text='Подпишись на канал', url='https://t.me/+MQWs5W2aQVI0MDEy') #ссылка на канал
+btnUrlChannel = InlineKeyboardButton(text='Подпишись на канал', url='https://t.me/+MQWs5W2aQVI0MDEy')
+btnUrlChannel2 = InlineKeyboardButton(text='Наш второй канал', url='https://t.me/cult_films20_21')
 btnDoneSub = InlineKeyboardButton(text='Я ПОДПИСАЛСЯ👍', callback_data='subchanneldone')
 
-checkSubMenu = InlineKeyboardMarkup(inline_keyboard=[[btnUrlChannel], [btnDoneSub]])
+checkSubMenu = InlineKeyboardMarkup(inline_keyboard=[[btnUrlChannel], [btnDoneSub],[btnUrlChannel2]])
