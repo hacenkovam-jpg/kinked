2269217546a0265be68531294678ab53cf1e7e8e
hacenkovam-jpg <hacenkovam@gmail.com>
2026-09-10 00:25:27 +0300

    update main

--- probbot/main.py
+++ probbot/main.py
@@ -124,6 +124,8 @@ async def bot_message(message: types.Message):
                  await message.answer('🍿Код: 018 \n<b>Моя жизнь 1993</b>', parse_mode='HTML')
             elif message.text == '019':
                  await message.answer('🍿Код: 019 \n<b>Соло 1996</b>', parse_mode='HTML')
+            elif message.text == '020':
+                 await message.answer('🍿Код: 020 \n<b>Побег из Могадишо</b>', parse_mode='HTML')
 
             elif message.text == '104':
                 await message.answer('🍿 Код: 104 \n<b>Герда</b>', parse_mode='HTML')
